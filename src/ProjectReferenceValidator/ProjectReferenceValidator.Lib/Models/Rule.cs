41dd305a6a39fe94f0d8f36ce5cc01e1df858645
Jonas Aklin <jonas.aklin@faigle.ch>
2021-05-24 21:11:18 +0200

    Added doc

--- src/ProjectReferenceValidator/ProjectReferenceValidator.Lib/Models/Rule.cs
+++ src/ProjectReferenceValidator/ProjectReferenceValidator.Lib/Models/Rule.cs
@@ -1,10 +1,24 @@
 ﻿
 namespace ProjectReferenceValidator.Lib.Models
 {
+    /// <summary>
+    /// Specifies a rule with respect to two projects; project A and project B.
+    /// </summary>
     public class Rule
     {
+        /// <summary>
+        /// Gets or sets project A, i.e. a regular expression to match a project or a set of projects.
+        /// </summary>
         public string ProjectA { get; set; }
+
+        /// <summary>
+        /// Gets or sets project B, i.e. a regular expression to match a project or a set of projects.
+        /// </summary>
         public string ProjectB { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rule type which describes the relationship between any given project A and B (e.g. "A must not reference B").
+        /// </summary>
         public RuleType Type { get; set; }
     }
 }
