00c5b49473be9a75ba45e70a773fe715543e8e1a
Jonas Aklin <jonas.aklin@faigle.ch>
2021-05-24 21:13:22 +0200

    Added doc

--- src/ProjectReferenceValidator/ProjectReferenceValidator.Lib/Models/Ruleset.cs
+++ src/ProjectReferenceValidator/ProjectReferenceValidator.Lib/Models/Ruleset.cs
@@ -2,8 +2,14 @@
 
 namespace ProjectReferenceValidator.Lib.Models
 {
+    /// <summary>
+    /// Represents a set of rules which can be applied to a project reference graph.
+    /// </summary>
     public class Ruleset
     {
+        /// <summary>
+        /// Gets or sets the rules in the ruleset.
+        /// </summary>
         public List<Rule> Rules { get; set; }
     }
 }
